a9c7712dd864d40b40eae66f5e6dab90395f4535
root <mohanedesmailux@gmail.com>
2025-01-01 10:43:42 +0200

    swarm

--- src/index.js
+++ src/index.js
@@ -56,7 +56,7 @@ app.get('/test-redis', (req, res) => {
 // Basic route to check if the server is up
 app.get('/', (req, res) => {
   console.log('traffic from ${os.hostname}')
-  res.send('Hello, this is the Node.js app running inside Docker! and also using docker hub');
+  res.send('Hello, this is the Node.js app running inside Docker! and also using docker hub and u can see changes by watchtoer feature ');
 });
 
 // Start the server
